3fe3ca6396dbfc1bb88d4d740dc9918af7d1e961
connick <hs.oh@ilts.co.kr>
2018-07-06 01:17:17 +0900

    Initial commit

---
+++ src/main/java/com/ex/board/JavaBoardApplication.java
@@ -0,0 +1,12 @@
+package com.ex.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBoardApplication.class, args);
+    }
+}
